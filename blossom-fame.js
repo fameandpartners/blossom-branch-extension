1b18df79ff6e4c777d13f8b0c918f978ccd6c53d
Adam <elgrecode@gmail.com>
2017-03-05 23:08:08 -0800

    adds loader

--- blossom-fame.js
+++ blossom-fame.js
@@ -8,6 +8,7 @@
     var GIT_BRANCH_ID_LENGTH = 5;
     var SAMPLE_BRANCH_ENDING = '-sample-branch-name';
     var WRAPPER_NODE = 'fame-blossom-wrapper';
+    var BRANCH_CONTENT_ID = '#fame-branch-content';
     var $ = document.querySelector.bind(window.document);
 
     function getTicketId(){
@@ -16,9 +17,9 @@
       return matchArr !== null ? matchArr[1] : '';
     }
 
-    function createNodeWithContent(content){
+    function createNode(){
       var div = document.createElement('div');
-      var template = "<span class='x-card-collaborators-header'>Suggested branch name</span><pre>{{content}}</pre>"
+      var template = "<span class='x-card-collaborators-header'>Suggested branch name</span><div id='fame-branch-content'></div>"
       var htmlStr = Mustache.to_html(template, {content: content});
 
       div.innerHTML = htmlStr;
@@ -26,19 +27,26 @@
       return div;
     }
 
+    function attachLoader(domNode){
+      domNode.innerHTML = "<div class='loader'><div class='line-scale'><div></div><div></div><div></div><div></div><div></div></div></div>";
+    }
+
     function titleFilter(titleWord){
       return (titleWord && titleWord.length > 2);
     }
 
     function formatBranchNameEnding(title){
-      // Pattern "-some-branch-name"
+      // Pattern: "-some-branch-name"
       // From: Some Branch Name - A/B Test
-      var splitTitleArr = title.split(' ').map(function(t){
+      var titleArr = title.split(' ').map(function(t){
         return t.replace(/[^a-z0-9]/gi,''); // Remove gnar chars
-      });
-      var filteredTitleArr = splitTitleArr.filter(titleFilter);
-      if (filteredTitleArr.length >= 3){
-        return '-' + filteredTitleArr.slice(0, 3).join('-');
+      }).filter(titleFilter);
+
+      if (titleArr.length === 2){
+        return '-' + titleArr.slice(0, 2).join('-');
+      }
+      if (titleArr.length >= 3){
+        return '-' + titleArr.slice(0, 3).join('-');
       }
 
       return SAMPLE_BRANCH_ENDING;
@@ -53,41 +61,54 @@
 
     // Event delegation helper
     function on(elSelector, eventName, selector, fn) {
-        var element = $(elSelector);
+      var element = $(elSelector);
 
-        element.addEventListener(eventName, function(event) {
-            var possibleTargets = element.querySelectorAll(selector);
-            var target = event.target;
+      element.addEventListener(eventName, function(event) {
+        var possibleTargets = element.querySelectorAll(selector);
+        var target = event.target;
 
-            for (var i = 0, l = possibleTargets.length; i < l; i++) {
-                var el = target;
-                var p = possibleTargets[i];
+        for (var i = 0, l = possibleTargets.length; i < l; i++) {
+          var el = target;
+          var p = possibleTargets[i];
 
-                while(el && el !== element) {
-                    if (el === p) {
-                        return fn.call(p, event);
-                    }
-                    el = el.parentNode;
-                }
-            }
-        });
+          while(el && el !== element) {
+              if (el === p) {
+                return fn.call(p, event);
+              }
+              el = el.parentNode;
+          }
+        }
+      });
     }
 
     // Card click event handler
     // NOTE: This is probably the most fragile part of extension relying on (4) class/id structure
     on('#board', 'click', '.card', function(e) {
-       // this function is only called, when a list item with 'yes' class is called
-       setTimeout(function(){
-         // Grab card element and inject suggested branch name
-         var applicationCard = $('.q-application-card');
-         if (applicationCard){
-           var cardSidebar = $('.x-card-sidebar-content-wrapper');
-           var cardTitle = $('.x-editable-text-content');
-           var ticketId = getTicketId().substr(0, GIT_BRANCH_ID_LENGTH);
-           var branchName = ticketId + extractBranchNameFromTitleNode(cardTitle);
-           cardSidebar.insertBefore(createNodeWithContent(branchName), cardSidebar.firstChild);
-         }
-       }, 2500);
+      setTimeout(function(){ // Placeholder
+        var applicationCard = $('.q-application-card');
+        var cardSidebar = $('.x-card-sidebar-content-wrapper');
+        if (applicationCard && cardSidebar){
+          cardSidebar.insertBefore(createNode(), cardSidebar.firstChild);
+          attachLoader($(BRANCH_CONTENT_ID));
+        }
+      }, 1000);
+
+
+      setTimeout(function(){
+        var fameBranchContent = $(BRANCH_CONTENT_ID);
+        if (!fameBranchContent){ return null; }
+
+        // Grab card element and inject suggested branch name
+        var cardTitle = $('.x-editable-text-content');
+        var ticketId = getTicketId().substr(0, GIT_BRANCH_ID_LENGTH);
+        var branchName = ticketId + extractBranchNameFromTitleNode(cardTitle);
+        var template = "<pre>{{branchName}}</pre>"
+
+        fameBranchContent.innerHTML = Mustache.to_html(
+          template,
+          {branchName: branchName}
+        );
+      }, 2500);
    });
 
 })(window);
